bea4b1a9d438a0d0d97f63dae50b7553f73df0ab
Gleb Ershov <hostgame@list.ru>
2020-11-17 23:23:14 +0600

    loop var is depricated

--- scanner.py
+++ scanner.py
@@ -1,22 +1,22 @@
 import asyncio
 from logger import app_logger as logger
 
-async def check_port(ip, port, loop):
+async def check_port(ip, port):
     logger.info("Check {} on {}".format(port, ip))
-    conn = asyncio.open_connection(ip, port, loop=loop)
+    conn = asyncio.open_connection(ip, port)
     try:
         reader, writer = await asyncio.wait_for(conn, timeout=3)
         return {"port": port, "state": "open"}
     except:
         return {"port": port, "state": "close"}
 
-async def scan(ip, begin, end, loop):
-    tasks = [asyncio.ensure_future(check_port(ip, p, loop)) for p in range(begin, end)]
+async def scan(ip, begin, end):
+    tasks = [asyncio.ensure_future(check_port(ip, p)) for p in range(begin, end)]
     responses = await asyncio.gather(*tasks)
     return responses
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    future = asyncio.ensure_future(scan('127.0.0.1', 8075, 8085, loop))
+    future = asyncio.ensure_future(scan('127.0.0.1', 8075, 8085))
     loop.run_until_complete(future)
     print(future.result())
